37c6dbc44cebaf4bb250b42787c3a363e118225f
Nicole Liang <nicolenliang@gmail.com>
2021-06-29 17:46:01 -0400

    add in Compose button

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -6,8 +6,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -59,6 +62,27 @@ public class TimelineActivity extends AppCompatActivity
                 finish(); // return back to Login screen
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // inflate the menu; adds items to action if present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == R.id.compose)
+        {
+            // compose icon has been tapped
+            Toast.makeText(this, "Compose!", Toast.LENGTH_SHORT).show();
+            // navigate to compose activity
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void populateHomeTimeline() // API REQUEST
