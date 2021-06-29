565ea02ce65aa29454151cc352d86b8c50ebec2b
Nicole Liang <nicolenliang@gmail.com>
2021-06-29 19:50:16 -0400

    include parceler

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -4,12 +4,18 @@ import android.util.JsonReader;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
+@Parcel
 public class User
 {
     public String name;
     public String screenName;
     public String publicImgURL;
+
+    // empty constructor for Parceler lib
+    public User() {}
+
     public static User fromJson(JSONObject jsonObject) throws JSONException
     {
         User user = new User();
